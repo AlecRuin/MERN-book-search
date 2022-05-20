4eee91ec642cc508048f51c549362be662aadfe3
Alec Harvey <ruinhard@gmail.com>
2022-05-19 23:23:11 -0500

    Updates

---
+++ precache-manifest.c895cd3b3fb6e751330307e3cade44d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6312e8566611e4b099a689b9212d072",
+    "url": "/MERN-book-search/index.html"
+  },
+  {
+    "revision": "ef6c5c3c88baef87f8d1",
+    "url": "/MERN-book-search/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "72c23c350b91dd1ac974",
+    "url": "/MERN-book-search/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "ef6c5c3c88baef87f8d1",
+    "url": "/MERN-book-search/static/js/2.de190500.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MERN-book-search/static/js/2.de190500.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72c23c350b91dd1ac974",
+    "url": "/MERN-book-search/static/js/main.2189f3e2.chunk.js"
+  },
+  {
+    "revision": "684e840a463dbdfe6b81",
+    "url": "/MERN-book-search/static/js/runtime-main.34e0ef18.js"
+  }
+]);
